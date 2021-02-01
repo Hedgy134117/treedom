2b9692552cd543dd157d69fe21899eab03bb94e2
Hedgy134117 <hedgy134117@gmail.com>
2021-02-01 16:16:07 -0500

    tree creation

--- app/home/script.js
+++ app/home/script.js
@@ -24,26 +24,26 @@ function loadTrees() {
         const assignedCon = document.querySelector('.assigned .container .boxes');
         for (let i = 0; i < assigned.length; i++) {
             const tree = assigned[i];
-            assignedCon.insertAdjacentHTML('beforeend', `
-            <div class="treeBox" onclick="window.location = '../tree/index.html?id=${tree.id}';">
-                <p class="treeBox__title">${tree.name}</p>
-                <p class="treeBox__text">Created by: ${tree.creator}</p>
-            </div>
-            `);
+            assignedCon.insertAdjacentHTML('beforeend', DOMCreateTree(true, tree.id, tree.name, tree.creator));
         }
         const createdCon = document.querySelector('.created .container .boxes');
         for (let i = 0; i < created.length; i++) {
             const tree = created[i];
-            createdCon.insertAdjacentHTML('beforeend', `
-            <div class="treeBox" onclick="window.location = '../tree/index.html?id=${tree.id}';">
-                <p class="treeBox__title">${tree.name}</p>
-                <p class="treeBox__text">Assigned to: ${tree.user}</p>
-            </div>
-            `);
+            createdCon.insertAdjacentHTML('beforeend', DOMCreateTree(false, tree.id, tree.name, tree.user));
         }
 
         return;
     })
+}
+
+function DOMCreateTree(assigned, id, name, user) {
+    let text = assigned ? 'Assigned to:' : 'Created by:';
+    return `
+    <div class="treeBox" onclick="window.location = '../tree/index.html?id=${id}';">
+        <p class="treeBox__title">${name}</p>
+        <p class="treeBox__text">${text} ${user}</p>
+    </div>
+    `;
 }
 
 function loadUsersIntoPopup() {
@@ -57,18 +57,44 @@ function loadUsersIntoPopup() {
     });
 }
 
+function openPopup() {
+    document.querySelector('#overlay').style.display = 'block';
+    document.querySelector('.popup').style.display = 'block';
+}
+
+function closePopup() {
+    document.querySelector('#overlay').style.display = 'none';
+    document.querySelector('.popup').style.display = 'none';
+}
+
 window.addEventListener('load', () => {
     login();
     loadTrees();
     loadUsersIntoPopup();
 
-    document.querySelector('.treeBox').addEventListener('click', () => {
-        document.querySelector('#overlay').style.display = 'block';
-        document.querySelector('.popup').style.display = 'block';
-    });
+    document.querySelector('.treeBox').addEventListener('click', openPopup);
 
-    document.querySelector('#overlay').addEventListener('click', () => {
-        document.querySelector('#overlay').style.display = 'none';
-        document.querySelector('.popup').style.display = 'none';
-    });
+    document.querySelector('#overlay').addEventListener('click', closePopup);
+
+    let form = document.querySelector('.popup form');
+    form.onsubmit = async (e) => {
+        e.preventDefault();
+        let data = new FormData(document.querySelector('.popup form'));
+        let name = data.get('name');
+        let user = data.get('user');
+        await treeAPI.createTree(username, password, name, user).then(data => {
+            const assignedCon = document.querySelector('.assigned .container .boxes');
+            const createdCon = document.querySelector('.created .container .boxes');
+            if (data.user == data.creator) {
+                assignedCon.insertAdjacentHTML('beforeend', DOMCreateTree(true, data.id, data.name, data.creator));
+                createdCon.insertAdjacentHTML('beforeend', DOMCreateTree(false, data.id, data.name, data.user));
+            }
+            else {
+                createdCon.insertAdjacentHTML('beforeend', DOMCreateTree(false, data.id, data.name, data.user));
+            }
+
+            form.reset();
+            closePopup();
+        });
+    }
 })
